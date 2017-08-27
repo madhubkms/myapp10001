b0780d7bcccc1961bb873fd3cda4411230f4c32c
Madhusudhan <madhu.drupal@gmail.com>
2017-08-28 01:40:45 +0530

    Created entries in student table.

--- src/main/resources/sql/postgres.sql
+++ src/main/resources/sql/postgres.sql
@@ -1,3 +1,14 @@
 create database myapp10001 with encoding='UTF8' owner=postgres template=template1 connection limit=-1 tablespace=pg_default;
 create user madhu with password 'madhu123';
 grant all privileges on database myapp10001 to madhu;
+
+create table student (
+    rollnumber serial primary key,
+    name varchar(50) NOT NULL
+);
+
+INSERT INTO student (NAME)
+VALUES ( 'ABC');
+
+INSERT INTO student (NAME)
+VALUES ( 'DEF');
